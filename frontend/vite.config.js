38b2c363b333b16383f1ad92071060e4808746d7
Dushan-S <dushanshashikelum@gmail.com>
2026-06-23 17:38:20 +0530

    fix: add external modules to vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,6 +8,11 @@ export default defineConfig({
     port: 5173,
     allowedHosts: ['localhost', '127.0.0.1', '172.20.10.2', '9f4287672efa.ngrok-free.app', 'bd3b8d572dfb.ngrok-free.app'],
   },
+  build: {
+    rollupOptions: {
+      
+      external: ['fs', 'path', 'os', 'child_process', 'util', 'stream', 'tty']
+    }
+  }
 })
-
 
