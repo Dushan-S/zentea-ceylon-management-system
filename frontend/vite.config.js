1848b3ea032177fba7675cda9031fdb2f66e92ce
Dushan-S <dushanshashikelum@gmail.com>
2026-06-23 17:46:55 +0530

    update vite config to fix build error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,28 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+    
+      fs: '',
+      path: '',
+      os: '',
+      child_process: '',
+      util: '',
+      stream: '',
+      tty: ''
+    },
+  },
   server: {
     host: true,
     port: 5173,
-    allowedHosts: ['localhost', '127.0.0.1', '172.20.10.2', '9f4287672efa.ngrok-free.app', 'bd3b8d572dfb.ngrok-free.app'],
+    allowedHosts: [
+      'localhost', 
+      '127.0.0.1', 
+      '172.20.10.2', 
+      '9f4287672efa.ngrok-free.app', 
+      'bd3b8d572dfb.ngrok-free.app'
+    ],
   },
   build: {
     rollupOptions: {
@@ -15,4 +33,3 @@ export default defineConfig({
     }
   }
 })
-
